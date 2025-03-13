d436d87f6721607445db024e4f0396a18f4ee173
jiahewu <jiahewu@usc.edu>
2025-03-13 00:56:30 -0700

    update instruction and data memory

--- Lab8/ise/data_mem_64.v
+++ Lab8/ise/data_mem_64.v
@@ -9,7 +9,7 @@ module data_mem_64(
 );
 
 reg [63: 0] data [255: 0];
-wire [63:0] memindex;
+wire [7:0] memindex;
 
 assign memindex = address>>3;
 
@@ -18,8 +18,19 @@ always @(*) begin
       2'b00: begin
         out_mem = 64'b0;
       end
+      //lw
       2'b01: begin
         out_mem = data[memindex];
+      end
+      //lh
+      2'b10: begin
+        if(read_mem[2]) out_mem = {32'b0, data[memindex][31:0]};
+        else out_mem = {{32{data[memindex][31]}}, data[memindex][31:0]};
+      end
+      //lb
+      2'b11: begin
+        if(read_mem[2]) out_mem = {56'b0, data[memindex][7:0]};
+        else out_mem = {{56{data[memindex][7]}}, data[memindex][7:0]};
       end
       default: begin
         out_mem = 64'b0;
@@ -28,34 +39,33 @@ always @(*) begin
 end
 
 always @(posedge clk) begin
-    if(rst) begin
-        integer i;
-        for(i=0;i<256;i=i+1) begin
-            data[i] <= 64'b0;
-        end
-    end else begin
       case (write_mem)
         2'b01: begin
         data[memindex] <= write_data;
+        end
+        2'b10: begin
+        data[memindex][31:0] <= write_data[31:0];
+        end
+        2'b11: begin
+        data[memindex][7:0] <= write_data[7:0];
         end
         default: begin
 
         end
       endcase
-    end
 end
 
 initial begin
-    data[0]=64'h0000_0000_0000_0143;
-    data[1]=64'h0000_0000_0000_007B;
-    data[2]=64'h0000_0000_FFFF_FE39;
-    data[3]=64'h0000_0000_0000_0002;
-    data[4]=64'h0000_0000_0000_0062;
-/*    data[5]=64'h0000_0000_
-    data[6]=64'h0000_0000_
-    data[7]=64'h0000_0000_
-    data[8]=64'h0000_0000_
-    data[9]=64'h0000_0000_*/
+    data[0]=64'h0000_0000_0000_0143; //323
+    data[1]=64'h0000_0000_0000_007B; //123
+    data[2]=64'h0000_0000_FFFF_FE39; //-455
+    data[3]=64'h0000_0000_0000_0002; //2
+    data[4]=64'h0000_0000_0000_0062; //98
+    data[5]=64'h0000_0000_0000_007D; //125
+    data[6]=64'h0000_0000_0000_000A; //10
+    data[7]=64'h0000_0000_0000_0041; //65
+    data[8]=64'h0000_0000_FFFF_FFC8; //-56
+    data[9]=64'h0000_0000_0000_0000; //0
 end
 
 endmodule
